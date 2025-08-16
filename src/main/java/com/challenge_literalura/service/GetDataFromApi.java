d569b00b6180bda887187dcf2d715b20f9a9fb36
franco azame <azamefranco@gmail.com>
2025-08-16 12:21:09 -0300

    ADD: añadi las los valores en application.properties para la base de datos, añadi la clase GetDataFromApi para las peticiones a gutendex

---
+++ src/main/java/com/challenge_literalura/service/GetDataFromApi.java
@@ -0,0 +1,9 @@
+package com.challenge_literalura.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetDataFromApi {
+    private final String apiUrl = "https://gutendex.com/";
+
+}
