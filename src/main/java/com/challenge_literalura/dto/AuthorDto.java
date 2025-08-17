cbdf1741ad0fade6a955dd0ada26f0e7233d3090
franco azame <azamefranco@gmail.com>
2025-08-16 23:13:49 -0300

    update: actualice el dto de autor

--- src/main/java/com/challenge_literalura/dto/AuthorDto.java
+++ src/main/java/com/challenge_literalura/dto/AuthorDto.java
@@ -1,12 +1,15 @@
 package com.challenge_literalura.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthorDto {
     private Long id;
     private String name;
+    @JsonProperty("birth_year")
     private Integer birthYear;
+    @JsonProperty("death_year")
     private Integer deathYear;
 
     public String getName() { return name; }
