61f9c6119943845da6983969a53a9f5ec57c1528
franco azame <azamefranco@gmail.com>
2025-08-17 01:31:27 -0300

    update: añadir validaciones extra en la seleccion de las opciones para que no se rompa la app

--- src/main/java/com/challenge_literalura/ChallengeLiteraluraApplication.java
+++ src/main/java/com/challenge_literalura/ChallengeLiteraluraApplication.java
@@ -34,9 +34,24 @@ public class ChallengeLiteraluraApplication {
 				4 - Listar autores vivos en un año determinado
 				5 - Listar libros por idioma
 				0 - Salir
-				""");
-			int optionSelected = sc.nextInt();
-			sc.nextLine();
+			""");
+			int optionSelected = -1;
+			boolean inputOptionValid = false;
+
+			while (!inputOptionValid) {
+				try {
+					String input = sc.nextLine();
+					optionSelected = Integer.parseInt(input);
+
+					if (optionSelected >= 0 && optionSelected <= 5) {
+						inputOptionValid = true;
+					} else {
+						System.out.println("Opción no válida. Ingrese un número entre 0 y 5");
+					}
+				} catch (NumberFormatException e) {
+					System.out.println("Debe ingresar un número. Intente nuevamente");
+				}
+			}
 
 			try {
 				if(optionSelected == 1){
@@ -77,8 +92,18 @@ public class ChallengeLiteraluraApplication {
 					});
 				} else if (optionSelected == 4) {
 					System.out.println("Escriba el año");
-					Integer year = sc.nextInt();
-					sc.nextLine();
+					Integer year = null;
+					boolean inputValid = false;
+
+					while (!inputValid) {
+						try {
+							String input = sc.nextLine();
+							year = Integer.parseInt(input);
+							inputValid = true;
+						} catch (NumberFormatException e) {
+							System.out.println("Debe ingresar un número válido. Intente nuevamente:");
+						}
+					}
 
 					List<AuthorDto> response = authorService.getAliveAuthorsByYear(year);
 
